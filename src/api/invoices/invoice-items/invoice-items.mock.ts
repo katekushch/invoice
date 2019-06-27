b6484dbe17908fdcd53f0b03911dc6c4af2f6bf7
Kate Kushch <katya@2muchcoffee.com>
2019-06-27 19:49:32 +0300

    feat(invoices:invoice-items): update invoice items mock

--- src/api/invoices/invoice-items/invoice-items.mock.ts
+++ src/api/invoices/invoice-items/invoice-items.mock.ts
@@ -7,5 +7,17 @@ export const invoiceItems: InvoiceItemInterface[] = [
     invoice_id: 1,
     product_id: 1,
     quantity: 2,
+  }),
+  new InvoiceItems({
+    _id: 2,
+    invoice_id: 1,
+    product_id: 222,
+    quantity: 222,
+  }),
+  new InvoiceItems({
+    _id: 3,
+    invoice_id: 2,
+    product_id: 333,
+    quantity: 333,
   })
 ];
