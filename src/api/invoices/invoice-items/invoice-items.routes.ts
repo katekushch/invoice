debb362f953c81fbde1bf6247ff23ee748a656ba
Kate Kushch <katya@2muchcoffee.com>
2019-06-28 13:49:16 +0300

    refactor(invoices:invoice-items): remove unnecessary code

--- src/api/invoices/invoice-items/invoice-items.routes.ts
+++ src/api/invoices/invoice-items/invoice-items.routes.ts
@@ -7,21 +7,8 @@ import {
   postInvoiceItem,
   putInvoiceItem
 } from './invoice-items.controllers';
-import { invoices } from '../invoices.mock';
-import { createError } from '../../../utils/createError';
 
 const invoiceItemsRouts = Router({mergeParams: true});
-
-invoiceItemsRouts.use((req, res, next) => {
-  const invoiceId = Number(req.params.invoice_id);
-  const foundedInvoice = invoices.find((invoice) => invoice._id === invoiceId);
-  if (foundedInvoice) {
-    next();
-  } else {
-    return res.status(404).send(createError(404, 'Invoice not found!'));
-  }
-});
-
 
 invoiceItemsRouts.get('/', getInvoiceItems);
 invoiceItemsRouts.post('/', postInvoiceItem);
