0a51e18e23587fe730b6b3b93cd7b65c3a89ab40
Kate Kushch <katya@2muchcoffee.com>
2019-06-28 15:15:06 +0300

    refactor(invoices): refactor functions params

--- src/api/invoices/invoices.controllers.ts
+++ src/api/invoices/invoices.controllers.ts
@@ -21,24 +21,21 @@ export function postInvoices(req, res, next) {
 }
 
 export function getInvoice(req, res, next) {
-  const invoiceIndex = req.entityIndex;
-  getInvoiceFromDB(invoiceIndex)
+  getInvoiceFromDB(req.entityIndex)
   .then((invoice) => res.json(invoice))
   .catch(next)
 }
 
 export function putInvoice(req, res, next) {
   const updatedOptions: Partial<InvoiceInterface> = req.body;
-  const invoiceIndex = req.entityIndex;
-  updateInvoiceInDB(invoiceIndex, updatedOptions)
+  updateInvoiceInDB(req.entityIndex, updatedOptions)
   .then((invoice) => res.json(invoice))
   .catch(next)
 }
 
 export function deleteInvoice(req, res, next) {
-  const invoiceIndex = req.entityIndex;
   const invoiceId = Number(req.params.id);
-  deleteInvoiceFromDB(invoiceIndex, invoiceId)
+  deleteInvoiceFromDB(req.entityIndex, invoiceId)
   .then((response) => res.status(204).send(response))
   .catch(next)
 }
