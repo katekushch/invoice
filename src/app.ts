a89ed92c4af0bf6f24b62a2f32928d45f5038db4
Kate Kushch <katya@2muchcoffee.com>
2019-07-01 19:28:29 +0300

    feat(app.ts): add mongoose connect

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import { initServer } from './server';
-import {initRouts} from './routes';
+import { initRouts } from './routes';
+//import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
 const bodyParser = require('body-parser');
 
@@ -10,11 +12,18 @@ const errorHandler = require('errorhandler');
 
 const app = express();
 
+mongoose.connect(`${process.env.DATABASE_URL}`, {useNewUrlParser: true})
+.then(() => console.log(`Connected to ${process.env.DATABASE_URL}`))
+.catch(() => {
+  console.log(`MongoDB connection error. Please make sure MongoDB is running.`);
+  process.exit()
+});
+
 initServer(app);
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 initRouts(app);
 
