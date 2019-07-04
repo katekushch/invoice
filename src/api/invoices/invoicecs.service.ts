795d6f33888157faf66de7ae4b03e432a2fda859
Kate Kushch <katya@2muchcoffee.com>
2019-07-04 11:00:26 +0300

    feat(invoices): add functionality for creating invoice items

--- src/api/invoices/invoicecs.service.ts
+++ src/api/invoices/invoicecs.service.ts
@@ -1,4 +1,5 @@
 import Invoice from './invoice.model';
+import InvoiceItem from './invoice-items/invoice-item.model';
 
 export function getInvoicesFromBD() {
   return Invoice.find();
@@ -6,6 +7,12 @@ export function getInvoicesFromBD() {
 
 export function addInvoiceToDB(invoice) {
   const newEntity = new Invoice(invoice);
+  invoice.items.map((invoiceItem) => {
+    invoiceItem.invoice_id = newEntity._id;
+    const newInvoiceItem = new InvoiceItem(invoiceItem);
+    newInvoiceItem.save();
+  });
+
   return newEntity.save();
 }
 
