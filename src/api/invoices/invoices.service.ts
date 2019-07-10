34da5ddfae0627ed3f100aecf24661021e202634
Kate Kushch <katya@2muchcoffee.com>
2019-07-10 18:39:15 +0300

    feat(invoices): create function for total counting

--- src/api/invoices/invoices.service.ts
+++ src/api/invoices/invoices.service.ts
@@ -1,4 +1,5 @@
-import {Promise as BBPromise} from 'bluebird'
+import mongoose from 'mongoose';
+import { Promise as BBPromise } from 'bluebird'
 
 import Invoice from './invoice.model';
 import InvoiceItem from './invoice-items/invoice-item.model';
@@ -11,11 +12,16 @@ export function getInvoicesFromBD() {
 export async function addInvoiceToDB(invoice) {
   const newEntity = new Invoice(invoice);
   await BBPromise.map(invoice.items, (invoiceItem: InvoiceItemInterface) => {
-      invoiceItem.invoice_id = newEntity._id;
-      const newInvoiceItem = new InvoiceItem(invoiceItem);
-      return newInvoiceItem.save();
-    });
-  return newEntity.save();
+    invoiceItem.invoice_id = newEntity._id;
+    const newInvoiceItem = new InvoiceItem(invoiceItem);
+    return newInvoiceItem.save();
+  });
+  await newEntity.save();
+  const total = await this.countTotal(newEntity._id, newEntity.discount);
+  return {
+    ...newEntity.toObject(),
+    total: total,
+  };
 }
 
 export async function updateInvoiceInDB(invoiceId, updatedOptions) {
@@ -41,4 +47,54 @@ export function deleteInvoiceFromDB(invoice) {
   return invoice.remove();
 }
 
+
+export async function countTotal(invoice_id, discount): Promise<number> {
+  const [total] = await InvoiceItem.aggregate([
+    {
+      $match: {invoice_id: mongoose.Types.ObjectId(invoice_id)}
+    },
+    {
+      $lookup: {
+        from: 'products',
+        localField: 'product_id',
+        foreignField: '_id',
+        as: 'product'
+      },
+    },
+    {$unwind: '$product'},
+    {
+      $addFields:
+        {
+          cost: {
+            $multiply: ['$product.price', '$quantity'],
+          },
+        }
+    },
+    {
+      $group: {
+        _id: invoice_id,
+        totalsArray: {$push: '$cost'}
+      }
+    },
+    {
+      $addFields: {
+        totalWithoutDiscount: {
+          $reduce: {
+            input: '$totalsArray',
+            initialValue: 0,
+            in: {$sum: ['$$value', '$$this']}
+          }
+        }
+      }
+    },
+    {
+      $project: {
+        total: {
+          $multiply: ['$totalWithoutDiscount', 1 - discount / 100]
+        },
+      }
+    }
+  ]);
+  return total.total;
+}
 
